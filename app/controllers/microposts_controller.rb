e2b9ca458ec6eccaac19f3579e774d2abe059e83
tbkoo53 <wokpoo@yahoo.co.jp>
2016-09-09 00:03:12 +0000

    表示の実装

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create]
+  before_action :logged_in_user, only: [:create, :destroy]
   
   def create
     @micropost = current_user.microposts.build(microposts_params)
@@ -9,6 +9,14 @@ class MicropostsController < ApplicationController
     else
       render 'static_pages/home'
     end  
+  end
+  
+  def destroy
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    return redirect_to root_url if @micropost.nil?
+    @micropost.destroy
+    flash[:sccess] = "Micropost deleted"
+    redirect_to request.referrer || root_url
   end
   
   private
